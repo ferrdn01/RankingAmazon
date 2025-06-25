bec11dc74cc39c033e936d06f10cd26de06d9df5
Daniel Ferreira <ferrdn01@gmail.com>
2025-06-25 14:36:45 -0300

    commit inicial

--- headers.py
+++ headers.py
@@ -1,6 +1,2 @@
-key = "6d93962c9dmsh69dee8195122e11p1cf13ajsnffabfabab59b"
-host = "real-time-amazon-data.p.rapidapi.com"
-
-
-if __name__ == "__main__":
-    print("oi")
+key = "minha chave"
+host = "meu host"
